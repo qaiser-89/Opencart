3f6b7656e33e2b6d5bd0de36012c2d7ae4ab4676
Muhammad Qaiser <muhammadqaiser@Muhammads-MBP.communityfibre.co.uk>
2025-01-12 18:34:45 +0000

    added test steps for error text verification

---
+++ src/main/java/Tests/RegisterNewUserPageFieldCheck.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import com.Base.BaseClass;
+
+public class RegisterNewUserPageFieldCheck  extends BaseClass
+{
+
+    String warning_Text="Warning: You must agree to the Privacy Policy!";
+    String firstname_errorText = "First Name must be between 1 and 32 characters!";
+    String lastname_errorText = "Last Name must be between 1 and 32 characters!";
+    String email_errorText= "E-Mail Address does not appear to be valid!";
+    String telephone_errorText= "Telephone must be between 3 and 32 characters!";
+    String password_errorText = "Password must be between 4 and 20 characters!";
+
+    
+
+
+}
--- src/main/java/Tests/RegisterNewUserTest.java
+++ src/main/java/Tests/RegisterNewUserTest.java
@@ -68,4 +68,6 @@ public class RegisterNewUserTest extends BaseClass
     }
 
 
+
+
 }
