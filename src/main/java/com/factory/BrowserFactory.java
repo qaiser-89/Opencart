236b8dcef9539e83bcb7f940d61a2bbb1c78e16f
Muhammad Qaiser <muhammadqaiser@Muhammads-MBP.communityfibre.co.uk>
2025-01-29 15:20:12 +0000

    back to starting of remote webdriver intigration Chrome working

--- src/main/java/com/factory/BrowserFactory.java
+++ src/main/java/com/factory/BrowserFactory.java
@@ -1,4 +1,5 @@
 package com.factory;
+
 import com.dataProvider.DataReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,7 +14,6 @@ import org.testng.Reporter;
 import javax.xml.crypto.Data;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.rmi.Remote;
 import java.time.Duration;
 
 public class BrowserFactory
@@ -25,54 +25,67 @@ public class BrowserFactory
         return driver;
     }
 
-    public static WebDriver getBrowser(String driverName, String URL) {
-
-        if (DataReader.readProperty("LocalEcecution").equalsIgnoreCase("True"))
+    public static WebDriver getBrowser(String driverName, String URL)
+    {
+        if(DataReader.readProperty("LocalEcecution").equalsIgnoreCase("True"))
         {
-            ChromeOptions options = new ChromeOptions();
-            if(DataReader.readProperty("Headless").equalsIgnoreCase("True")) {
+            ChromeOptions options=new ChromeOptions();
+            if(DataReader.readProperty("Headless").equalsIgnoreCase("True"))
+            {
                 options.addArguments("--headless=new");
+                Reporter.log("Chrome is running in Headless Mode!!, you can change the setting in Config file", true);
             }
-            driver = new ChromeDriver(options);
-            Reporter.log("Chrome is running in Headless Mode!!, you can change the setting in Config file");
-        } else
+            driver =new ChromeDriver();
+        }
+        else
         {
-            if (driverName.equalsIgnoreCase("Chrome") || driverName.equalsIgnoreCase("Google Chrome")) {
-                ChromeOptions options = new ChromeOptions();
+            if(driverName.equalsIgnoreCase("Chrome")|| driverName.equalsIgnoreCase("Google Chrome"))
+            {
+                ChromeOptions options=new ChromeOptions();
+                DesiredCapabilities capabilities=new DesiredCapabilities();
+                capabilities.setCapability("browserName","chrome");
+                capabilities.setCapability("browserVersion","132.0");
+                capabilities.setCapability("platformName","linux");
 
-                DesiredCapabilities capabilities = new DesiredCapabilities();
-
-                capabilities.setCapability("browserName", "chrome");
-                capabilities.setCapability("browserVersion", "132.0");
-                capabilities.setCapability("platformName", "linux");
-
-                if (DataReader.readProperty("Headless").equalsIgnoreCase("True")) {
+                if(DataReader.readProperty("Headless").equalsIgnoreCase("True"))
+                {
                     options.addArguments("--headless=new");
-                    Reporter.log("Chrome is running in Headless Mode!!, you can change the setting in Config file");
+                    Reporter.log("Chrome is running in Headless Mode!!, you can change the setting in Config file", true);
                 }
                 options.merge(capabilities);
 
+                //driver =new ChromeDriver();
                 try {
-                    driver = new RemoteWebDriver(new URL(DataReader.readProperty("HubURL")), options);
+                    driver = new RemoteWebDriver(new URL(DataReader.readProperty("HubURL")),options);
                 } catch (MalformedURLException e) {
-                    Reporter.log("could not connect to selenium Grid -seems hub is not reachable" + e.getMessage());
+                    Reporter.log("Could not Connect to Selenium Grid - Seems hub is not reachable "+e.getMessage());
                 }
-            } else if (driverName.equalsIgnoreCase("Firefox") || driverName.equalsIgnoreCase("Mozila Firefox")) {
+
+            }
+
+            else if (driverName.equalsIgnoreCase("Firefox")|| driverName.equalsIgnoreCase("Mozila Firefox"))
+            {
                 driver = new FirefoxDriver();
-            } else if (driverName.equalsIgnoreCase("safari") || driverName.equalsIgnoreCase("Apple Safari")) {
+            }
+            else if (driverName.equalsIgnoreCase("safari") || driverName.equalsIgnoreCase("Apple Safari"))
+            {
                 driver = new SafariDriver();
-            } else if (driverName.equalsIgnoreCase("Edge") || driverName.equalsIgnoreCase("Microsoft Edge")) {
-                driver = new EdgeDriver();
-            } else {
+            }
+            else if (driverName.equalsIgnoreCase("Edge")|| driverName.equalsIgnoreCase("Microsoft Edge"))
+            {
+                driver =new EdgeDriver();
+            }
+            else
+            {
                 System.out.println("We only support Safari, Edge, Chrome and Firefox currently");
             }
+
         }
         driver.manage().window().maximize();
         driver.get((URL));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         return driver;
     }
-
 
     public static void closeDriver()
     {
