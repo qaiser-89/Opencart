06851100f40efa00bda849354883e2908cac43be
Muhammad Qaiser <muhammadqaiser@Muhammads-MBP.communityfibre.co.uk>
2025-10-21 19:17:37 +0100

    modified properties for retry implementation

--- src/main/java/com/listener/RetryAnalyzer.java
+++ src/main/java/com/listener/RetryAnalyzer.java
@@ -3,11 +3,13 @@ package com.listener;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
+import com.dataProvider.DataReader;
+
 public class RetryAnalyzer implements IRetryAnalyzer
 {
 	
 	private int retryCount  = 0;
-	private int maxRetryCount = 2;
+	private int maxRetryCount = Integer.parseInt(DataReader.readProperty("retryCount"));
 
 	@Override
 	public boolean retry(ITestResult result) {
